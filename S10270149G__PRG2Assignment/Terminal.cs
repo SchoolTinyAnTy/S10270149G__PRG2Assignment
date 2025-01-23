24297c934918bdb24356d4d8c6afef55f6527ef9
Ming Kai Orlando Lee <s10270149@connect.np.edu.sg>
2025-01-23 10:34:36 +0800

    providing terminal class

---
+++ S10270149G__PRG2Assignment/Terminal.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10270149G__PRG2Assignment
+{
+    internal class Terminal
+    {
+    }
+}
